8961a4b1dfa904dbf4b1450da1b1db719d408c64
hikaru <hikaru.fujita.0903@gmail>
2019-05-09 12:11:10 +0900

    Finish_step11

---
+++ step11_binding/js/main.js
@@ -0,0 +1,12 @@
+const app = new Vue({
+  el: '#app',
+  data: {
+    message: 'hello, Vue.js!',
+    checked: false,
+    colors: [],
+    color: '',
+    selected: '',
+    selected2: [],
+    age: 0
+  }
+})
